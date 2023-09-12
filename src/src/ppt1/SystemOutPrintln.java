1fdfcf89e3b811db18f718a6fa84521fc782bb7a
mini <alchemist72@naver.com>
2023-09-12 12:43:27 +0900

    "hi"

---
+++ src/src/ppt1/SystemOutPrintln.java
@@ -0,0 +1,11 @@
+package src.ppt1;
+
+
+public class SystemOutPrintln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nice meet you Java!!");
+	}
+
+}
